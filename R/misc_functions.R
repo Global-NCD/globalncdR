f077a500f6af75ea5f27b7f5f62e280b640ee4a8
shossen1 <57533234+shossen1@users.noreply.github.com>
2021-03-01 14:46:25 -0500

    var.missmatch

---
+++ R/misc_functions.R
@@ -0,0 +1,8 @@
+
+var.missmatch = function(a,b){
+  print("Present in dataset A but absent in dataset B")
+  names(a)[!(names(a) %in% names(b))]
+  
+  print("Present in dataset B but absent in dataset A")
+  names(b)[!(names(b) %in% names(a))]
+}
