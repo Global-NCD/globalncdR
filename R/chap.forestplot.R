42652c13f16a95f8d1483e62854da5b546cbe06f
shossen1 <57533234+shossen1@users.noreply.github.com>
2021-09-30 13:08:25 -0400

    Update chap.forestplot.R

--- R/chap.forestplot.R
+++ R/chap.forestplot.R
@@ -115,22 +115,23 @@ tab2$face = ifelse(tab2$y %in% y.headings, "bold", "plain")
   return(p)
 }
 
-# data1 = dat %>% filter(visit > 0 & age_rand>=50)
-# data2 = dat %>% filter(visit > 0 & age_rand<50)
-# data3 = dat %>% filter(visit > 0 & lpg == 1)
-# data4 = dat %>% filter(visit > 0 & lpg == 0)
-# data5 = dat %>% filter(visit > 0 & sbp > 120)
-# data6 = dat %>% filter(visit > 0 & sbp <= 120)
+data1 = dat %>% filter(visit > 0 & age_rand>=50)
+data2 = dat %>% filter(visit > 0 & age_rand<50)
+data3 = dat %>% filter(visit > 0 & lpg == 1)
+data4 = dat %>% filter(visit > 0 & lpg == 0)
+data5 = dat %>% filter(visit > 0 & sbp > 120)
+data6 = dat %>% filter(visit > 0 & sbp <= 120)
 
-# tiff('CHAP_Healthstate_EQ5D.tiff', units="in", width=10, height=8, res=100, compression = 'lzw')
-# chap.forestplot(datalist = c("data1", "data2", "data3", "data4", "data5", "data6"),
-#                 var = "healthstate_eq5d",
-#                 y.lab = c("Age", " >= 50 y", "< 50 y",
-#                           "Owns LPG", "Yes", "No",
-#                           "SBP", ">= 120mm Hg", "< 120mm Hg"),
-#                 y.headings = c("Age", "Owns LPG", "SBP"),
-#                 title = "Healthstate EQ5D")
-# dev.off()
+tiff('CHAP_Healthstate_EQ5D.tiff', units="in", width=10, height=8, res=100, compression = 'lzw')
+chap.forestplot(datalist = c("data1", "data2", "data3", "data4", "data5", "data6"),
+                var = "healthstate_eq5d",
+                y.lab = c("Age", " >= 50 y", "< 50 y",
+                          "Owns LPG", "Yes", "No",
+                          "SBP", ">= 120mm Hg", "< 120mm Hg"),
+                y.headings = c("Age", "Owns LPG", "SBP"),
+                title = "Healthstate EQ5D")
+
+dev.off()
 
 
 
